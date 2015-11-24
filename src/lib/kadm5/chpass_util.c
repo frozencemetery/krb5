700f8d4b91f650b364d790f67f26b0dc517bf82f
Robbie Harwood <rharwood@redhat.com>
2015-11-24 15:39:13 -0500

    Clean up chpass_util.c

--- src/lib/kadm5/chpass_util.c
+++ src/lib/kadm5/chpass_util.c
@@ -16,10 +16,19 @@
 
 #define string_text error_message
 
+#ifdef ZEROPASSWD
+#define zmemset(a, b, c) \
+    if (a != NULL) \
+        memset(a, b, c);
+#else
+#define zmemset(a, b, c)
+#endif
+
 /*
  * Function: kadm5_chpass_principal_util
  *
- * Purpose: Wrapper around chpass_principal. We can read new pw, change pw and return useful messages
+ * Purpose: Wrapper around chpass_principal. We can read new pw, change pw and
+ * return useful messages
  *
  * Arguments:
  *
@@ -71,47 +80,40 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
 
     _KADM5_CHECK_HANDLE(server_handle);
 
-    if (ret_pw)
-        *ret_pw = NULL;
-
     if (new_pw != NULL) {
         new_password = new_pw;
     } else { /* read the password */
         krb5_context context;
 
-        if ((code = (int) kadm5_init_krb5_context(&context)) == 0) {
-            pwsize = sizeof(buffer);
-            code = krb5_read_password(context, KADM5_PW_FIRST_PROMPT,
-                                      KADM5_PW_SECOND_PROMPT,
-                                      buffer, &pwsize);
-            krb5_free_context(context);
+        code = (int) kadm5_init_krb5_context(&context);
+        if (code == KRB5_LIBOS_BADPWDMATCH) {
+            zmemset(buffer, 0, sizeof(buffer));
+            strncpy(msg_ret, string_text(CHPASS_UTIL_NEW_PASSWORD_MISMATCH),
+                    msg_len - 1);
+            msg_ret[msg_len - 1] = '\0';
+            return(code);
+        } else if (code != 0) {
+            zmemset(buffer, 0, sizeof(buffer));
+            snprintf(msg_ret, msg_len, "%s %s\n\n%s",
+                     error_message(code),
+                     string_text(CHPASS_UTIL_WHILE_READING_PASSWORD),
+                     string_text(CHPASS_UTIL_PASSWORD_NOT_CHANGED));
+            msg_ret[msg_len - 1] = '\0';
+            return(code);
         }
 
+        pwsize = sizeof(buffer);
+        code = krb5_read_password(context, KADM5_PW_FIRST_PROMPT,
+                                  KADM5_PW_SECOND_PROMPT,
+                                  buffer, &pwsize);
         if (code == 0)
             new_password = buffer;
-        else {
-#ifdef ZEROPASSWD
-            memset(buffer, 0, sizeof(buffer));
-#endif
-            if (code == KRB5_LIBOS_BADPWDMATCH) {
-                strncpy(msg_ret, string_text(CHPASS_UTIL_NEW_PASSWORD_MISMATCH),
-                        msg_len - 1);
-                msg_ret[msg_len - 1] = '\0';
-                return(code);
-            } else {
-                snprintf(msg_ret, msg_len, "%s %s\n\n%s",
-                         error_message(code),
-                         string_text(CHPASS_UTIL_WHILE_READING_PASSWORD),
-                         string_text(CHPASS_UTIL_PASSWORD_NOT_CHANGED));
-                msg_ret[msg_len - 1] = '\0';
-                return(code);
-            }
-        }
+        krb5_free_context(context);
+
         if (pwsize == 0) {
-#ifdef ZEROPASSWD
-            memset(buffer, 0, sizeof(buffer));
-#endif
-            strncpy(msg_ret, string_text(CHPASS_UTIL_NO_PASSWORD_READ), msg_len - 1);
+            /* pwsize is sizeof(buffer) */
+            strncpy(msg_ret, string_text(CHPASS_UTIL_NO_PASSWORD_READ),
+                    msg_len - 1);
             msg_ret[msg_len - 1] = '\0';
             return(KRB5_LIBOS_CANTREADPWD); /* could do better */
         }
@@ -122,37 +124,29 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
 
     code = kadm5_chpass_principal(server_handle, princ, new_password);
 
-#ifdef ZEROPASSWD
+    /* in case we read a new password */
     if (!ret_pw)
-        memset(buffer, 0, sizeof(buffer)); /* in case we read a new password */
-#endif
+        zmemset(buffer, 0, sizeof(buffer));
 
     if (code == KADM5_OK) {
-        strncpy(msg_ret, string_text(CHPASS_UTIL_PASSWORD_CHANGED), msg_len - 1);
+        strncpy(msg_ret, string_text(CHPASS_UTIL_PASSWORD_CHANGED),
+                msg_len - 1);
         msg_ret[msg_len - 1] = '\0';
         return(0);
-    }
-
-    if ((code != KADM5_PASS_Q_TOOSHORT) &&
-        (code != KADM5_PASS_REUSE) &&(code != KADM5_PASS_Q_CLASS) &&
-        (code != KADM5_PASS_Q_DICT) && (code != KADM5_PASS_TOOSOON)) {
+    } else if ((code != KADM5_PASS_Q_TOOSHORT) &&
+               (code != KADM5_PASS_REUSE) && (code != KADM5_PASS_Q_CLASS) &&
+               (code != KADM5_PASS_Q_DICT) && (code != KADM5_PASS_TOOSOON)) {
         /* Can't get more info for other errors */
         snprintf(msg_ret, msg_len, "%s\n%s %s\n",
                  string_text(CHPASS_UTIL_PASSWORD_NOT_CHANGED),
                  error_message(code),
                  string_text(CHPASS_UTIL_WHILE_TRYING_TO_CHANGE));
         return(code);
-    }
-
-    /* Ok, we have a password quality error. Return a good message */
-
-    if (code == KADM5_PASS_REUSE) {
+    } else if (code == KADM5_PASS_REUSE) {
         strncpy(msg_ret, string_text(CHPASS_UTIL_PASSWORD_REUSE), msg_len - 1);
         msg_ret[msg_len - 1] = '\0';
         return(code);
-    }
-
-    if (code == KADM5_PASS_Q_DICT) {
+    } else if (code == KADM5_PASS_Q_DICT) {
         strncpy(msg_ret, string_text(CHPASS_UTIL_PASSWORD_IN_DICTIONARY),
                 msg_len - 1);
         msg_ret[msg_len - 1] = '\0';
@@ -161,8 +155,8 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
 
     /* Look up policy for the remaining messages */
 
-    code2 = kadm5_get_principal (lhandle, princ, &princ_ent,
-                                 KADM5_PRINCIPAL_NORMAL_MASK);
+    code2 = kadm5_get_principal(lhandle, princ, &princ_ent,
+                                KADM5_PRINCIPAL_NORMAL_MASK);
     if (code2 != 0) {
         snprintf(msg_ret, msg_len, "%s %s\n%s %s\n\n%s\n",
                  error_message(code2),
@@ -172,9 +166,7 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
                  string_text(CHPASS_UTIL_PASSWORD_NOT_CHANGED));
         msg_ret[msg_len - 1] = '\0';
         return(code);
-    }
-
-    if ((princ_ent.aux_attributes & KADM5_POLICY) == 0) {
+    } else if ((princ_ent.aux_attributes & KADM5_POLICY) == 0) {
         /* Some module implements its own password policy. */
         snprintf(msg_ret, msg_len, "%s\n\n%s",
                  error_message(code),
@@ -184,10 +176,10 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
         return(code);
     }
 
-    code2 = kadm5_get_policy(lhandle, princ_ent.policy,
-                             &policy_ent);
+    code2 = kadm5_get_policy(lhandle, princ_ent.policy, &policy_ent);
     if (code2 != 0) {
-        snprintf(msg_ret, msg_len, "%s %s\n%s %s\n\n%s\n ", error_message(code2),
+        snprintf(msg_ret, msg_len, "%s %s\n%s %s\n\n%s\n ",
+                 error_message(code2),
                  string_text(CHPASS_UTIL_GET_POLICY_INFO),
                  error_message(code),
                  string_text(CHPASS_UTIL_WHILE_TRYING_TO_CHANGE),
@@ -202,19 +194,13 @@ kadm5_ret_t _kadm5_chpass_principal_util(void *server_handle,
         (void) kadm5_free_principal_ent(lhandle, &princ_ent);
         (void) kadm5_free_policy_ent(lhandle, &policy_ent);
         return(code);
-    }
-
-/* Can't get more info for other errors */
-
-    if (code == KADM5_PASS_Q_CLASS) {
+    } else if (code == KADM5_PASS_Q_CLASS) {
         snprintf(msg_ret, msg_len, string_text(CHPASS_UTIL_TOO_FEW_CLASSES),
                  policy_ent.pw_min_classes);
         (void) kadm5_free_principal_ent(lhandle, &princ_ent);
         (void) kadm5_free_policy_ent(lhandle, &policy_ent);
         return(code);
-    }
-
-    if (code == KADM5_PASS_TOOSOON) {
+    } else if (code == KADM5_PASS_TOOSOON) {
         time_t until;
         char *time_string, *ptr;
 
